3e3290b7410b77ca309197a5e220cfe337f24246
geunee20 <70561864+geunee20@users.noreply.github.com>
2023-05-06 12:34:23 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,39 +1,41 @@
-const defaultCenter = new kakao.maps.LatLng(33.450701, 126.570667);
-const mapContainer = document.getElementById("map");
-const mapOptions = {
-  center: defaultCenter,
-  level: 3,
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const defaultCenter = new kakao.maps.LatLng(33.450701, 126.570667);
+  const mapContainer = document.getElementById("map");
+  const mapOptions = {
+    center: defaultCenter,
+    level: 3,
+  };
 
-const map = new kakao.maps.Map(mapContainer, mapOptions);
+  const map = new kakao.maps.Map(mapContainer, mapOptions);
 
-if (!window.ReactNativeWebView) {
-  showUserLocation();
-}
-
-function addMarker(position) {
-  const marker = new kakao.maps.Marker({
-    position,
-  });
-  marker.setMap(map);
-}
-
-function showUserLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const userLatLng = new kakao.maps.LatLng(
-          position.coords.latitude,
-          position.coords.longitude
-        );
-        map.setCenter(userLatLng);
-        addMarker(userLatLng);
-      },
-      (error) => {
-        console.error("Error getting user location:", error);
-      }
-    );
-  } else {
-    console.error("Geolocation is not supported by this browser.");
+  function addMarker(position) {
+    const marker = new kakao.maps.Marker({
+      position,
+    });
+    marker.setMap(map);
   }
-}
+
+  function showUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLatLng = new kakao.maps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          map.setCenter(userLatLng);
+          addMarker(userLatLng);
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }
+
+  if (!window.ReactNativeWebView) {
+    showUserLocation();
+  }
+});
