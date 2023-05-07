87458f3370324626b40af3b7e0222fdf03a1f8d8
geunee20 <70561864+geunee20@users.noreply.github.com>
2023-05-07 12:13:06 -0400

    Update map.js

--- map.js
+++ map.js
@@ -16,88 +16,7 @@ try {
     longitude = urlParams.get("lon");
     distance = parseFloat(urlParams.get("distance"));
 
-    const overpassQuery = `[out:json][timeout:5];(way[highway~"^(residential|footway|cycleway)$"](around:${
-      (distance * 1000) / 4
-    },${latitude},${longitude});>;);out;`;
-
-    const response = await axios.get(
-      "https://overpass-api.de/api/interpreter",
-      {
-        params: { data: overpassQuery },
-      }
-    );
-    const geojson = osmtogeojson(response.data);
-    const roads = geojson.features.filter(
-      (feature) => feature.geometry.type === "LineString"
-    );
-
-    const allRoadSets = [];
-
-    for (let i = 0; i < 20; i++) {
-      const randomIndices = new Set();
-
-      while (randomIndices.size < Math.min(2, roads.length)) {
-        const randomIndex = Math.floor(Math.random() * roads.length);
-        randomIndices.add(randomIndex);
-      }
-
-      const randomRoads = Array.from(randomIndices).map(
-        (index) => roads[index]
-      );
-      const roadCoords = randomRoads.map((road) => {
-        const coordinates = road.geometry.coordinates;
-        const middleCoordinate =
-          coordinates[Math.floor(coordinates.length / 2)];
-
-        return {
-          latitude: middleCoordinate[1],
-          longitude: middleCoordinate[0],
-        };
-      });
-
-      const origin = {
-        latitude: currentLocation.latitude,
-        longitude: currentLocation.longitude,
-      };
-
-      const minDistance = distance * 0.1;
-
-      if (
-        euclideanDistanceInKm(origin, roadCoords[1]) >= minDistance &&
-        euclideanDistanceInKm(roadCoords[0], roadCoords[1]) >= minDistance &&
-        euclideanDistanceInKm(roadCoords[1], origin) >= minDistance
-      ) {
-        const euclideanDistance = totalEuclideanDistanceInKm(
-          origin,
-          roadCoords
-        );
-
-        allRoadSets.push({
-          roadCoords,
-          euclideanDistance: euclideanDistance,
-        });
-      } else {
-        // If the roads don't meet the criteria, decrement i to retry with another pair
-        i--;
-      }
-    }
-
-    allRoadSets.sort(
-      (a, b) =>
-        Math.abs(distance * 0.7 - a.euclideanDistance) -
-        Math.abs(distance * 0.7 - b.euclideanDistance)
-    );
-
-    const bestFiveRoadSets = allRoadSets.slice(0, 5);
-    center = new kakao.maps.LatLng(latitude, longitude);
-    addMarker(center);
-    map.setCenter(center);
-
-    const dataToSend = JSON.stringify({
-      distance: distance,
-      bestFiveRoadSets: bestFiveRoadSets,
-    });
-    window.ReactNativeWebView.postMessage(dataToSend);
+    fetchData();
   } else {
     showUserLocation();
   }
@@ -130,6 +49,82 @@ function showUserLocation() {
   } else {
     console.error("Geolocation is not supported by this browser.");
   }
+}
+
+async function fetchData() {
+  const overpassQuery = `[out:json][timeout:5];(way[highway~"^(residential|footway|cycleway)$"](around:${
+    (distance * 1000) / 4
+  },${latitude},${longitude});>;);out;`;
+
+  const response = await axios.get("https://overpass-api.de/api/interpreter", {
+    params: { data: overpassQuery },
+  });
+  const geojson = osmtogeojson(response.data);
+  const roads = geojson.features.filter(
+    (feature) => feature.geometry.type === "LineString"
+  );
+
+  const allRoadSets = [];
+
+  for (let i = 0; i < 20; i++) {
+    const randomIndices = new Set();
+
+    while (randomIndices.size < Math.min(2, roads.length)) {
+      const randomIndex = Math.floor(Math.random() * roads.length);
+      randomIndices.add(randomIndex);
+    }
+
+    const randomRoads = Array.from(randomIndices).map((index) => roads[index]);
+    const roadCoords = randomRoads.map((road) => {
+      const coordinates = road.geometry.coordinates;
+      const middleCoordinate = coordinates[Math.floor(coordinates.length / 2)];
+
+      return {
+        latitude: middleCoordinate[1],
+        longitude: middleCoordinate[0],
+      };
+    });
+
+    const origin = {
+      latitude: currentLocation.latitude,
+      longitude: currentLocation.longitude,
+    };
+
+    const minDistance = distance * 0.1;
+
+    if (
+      euclideanDistanceInKm(origin, roadCoords[1]) >= minDistance &&
+      euclideanDistanceInKm(roadCoords[0], roadCoords[1]) >= minDistance &&
+      euclideanDistanceInKm(roadCoords[1], origin) >= minDistance
+    ) {
+      const euclideanDistance = totalEuclideanDistanceInKm(origin, roadCoords);
+
+      allRoadSets.push({
+        roadCoords,
+        euclideanDistance: euclideanDistance,
+      });
+    } else {
+      // If the roads don't meet the criteria, decrement i to retry with another pair
+      i--;
+    }
+  }
+
+  allRoadSets.sort(
+    (a, b) =>
+      Math.abs(distance * 0.7 - a.euclideanDistance) -
+      Math.abs(distance * 0.7 - b.euclideanDistance)
+  );
+
+  const bestFiveRoadSets = allRoadSets.slice(0, 5);
+  center = new kakao.maps.LatLng(latitude, longitude);
+  addMarker(center);
+  map.setCenter(center);
+
+  const dataToSend = JSON.stringify({
+    distance: distance,
+    bestFiveRoadSets: bestFiveRoadSets,
+  });
+  window.ReactNativeWebView.postMessage(dataToSend);
 }
 
 function euclideanDistanceInKm(point1, point2) {
